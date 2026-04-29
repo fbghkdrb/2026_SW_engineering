e22e6d0acee522285aaae5342339cbfbdd956913
ChoiHyunJae <gusalsdk0926@naver.com>
2026-04-29 22:15:12 +0900

    chore: 프로젝트 초기 세팅

---
+++ backend/src/main/java/com/__SW_engineering/wordtama/WordtamaApplication.java
@@ -0,0 +1,13 @@
+package com.__SW_engineering.wordtama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordtamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordtamaApplication.class, args);
+	}
+
+}
