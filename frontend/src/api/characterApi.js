d1bf5993a162eb496c792d630478db82565721e2
junhwan1029123-hub <junhwan1029123@gmail.com>
2026-05-15 11:39:51 +0900

    Revert "Feature/quiz add"

--- frontend/src/api/characterApi.js
+++
@@ -1,5 +0,0 @@
-import axiosInstance from "./axiosInstance";
-
-export const getCharacter = () => axiosInstance.get("/api/character");
-
-export const reviveCharacter = () => axiosInstance.post("/api/character/revive");
